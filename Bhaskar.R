b87737449fc5ad7dd649c650aca57a9050850731
Matheus Senna <97397024+Matteuos@users.noreply.github.com>
2022-05-07 09:59:09 -0300

    importacao

---
+++ Bhaskar.R
@@ -0,0 +1,20 @@
+
+#ax^2+bx+c=0
+#put the values of abc you need
+
+a <- 1
+b <- 3
+c <- -4
+  
+delta = b^2-4*a*c
+delta
+
+x1 <- -b+sqrt(delta)
+print(x1)
+
+x2 <- -b-sqrt(delta)
+print(x2)
+
+
+
+
